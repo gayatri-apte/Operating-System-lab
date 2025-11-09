42559da14ab76b97dc83f21e6144f71a885d337e
gayatri-apte <aptegayatri28@gmail.com>
2025-11-10 01:05:56 +0530

    Add files via upload

---
+++ server.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+
+#define SHM_SIZE 1024
+
+int main()
+{
+    key_t key = ftok("shmfile", 65);
+    if(key == -1)
+    {
+        perror("ftok");
+        exit(1);
+    }
+    int shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
+    if(shmid == -1)
+    {
+        perror("shmid");
+        exit(1);
+    }
+
+    char*data = (char*)shmat(shmid, NULL, 0);
+    if(data == (char*)-1)
+    {
+        perror("shmat");
+        exit(1);
+    }
+    printf("Enter a message:\n");
+    fgets(data, SHM_SIZE, stdin);
+
+    if(shmdt(data) == -1)
+    {
+        perror("shmdt");
+        exit(1);
+    }
+    return 0;
+}
